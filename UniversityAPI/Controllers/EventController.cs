7f8a6fae50a1f03500ee2869d549c6eaa2e2bf8b
LorenzoMiechielsen <r0885689@student.thomasmore.be>
2025-01-02 16:44:41 +0100

    event

---
+++ UniversityAPI/Controllers/EventController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using University.DAL.Models;
+using University.DAL;
+
+namespace UniversityAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private readonly IRepository<Event> _eventRepository;
+
+        public EventController(IRepository<Event> eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Event>>> GetEvents()
+        {
+            var events = await _eventRepository.GetAsync(null, null, c => c.eventName);
+
+            if (events == null || !events.Any())
+            {
+                return NotFound("No events found.");
+            }
+
+            return Ok(events);
+        }
+    }
+
+}
